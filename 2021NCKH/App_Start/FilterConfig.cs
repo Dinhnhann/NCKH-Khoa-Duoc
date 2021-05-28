dfc9ca0c18c4d43825659eecadacaf1762956fc1
Đình Nhân <nh Nhn@LAPTOP-4OQO6M05>
2021-05-28 19:40:29 +0700

    Add project files.

---
+++ 2021NCKH/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace _2021NCKH
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ 2021NCKH/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_2021NCKH.MvcApplication" Language="C#" %>
